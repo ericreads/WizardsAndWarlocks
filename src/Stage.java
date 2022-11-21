808df7fa1f28c24e8b235911df8506af512e4a75
Eric <ericreads032@gmail.com>
2022-11-21 10:30:15 -0500

    Create Obstacle and Stage class

---
+++ src/Stage.java
@@ -0,0 +1,58 @@
+//Import required modules
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+public class Stage {
+	//list to store the obstacles
+	private Obstacle[][] obstacles;
+	//Int to store the width and height of the obstacles
+	private static int DIMENSION = 50;
+	public Stage(float initialPercent)
+	{
+		//Initialize the obstacles list
+		obstacles = new Obstacle[(1280-DIMENSION)/DIMENSION][(720-DIMENSION)/DIMENSION];
+		
+		//Create new obstacles for each array element
+		for(int i = 0; i < obstacles.length; i++)
+		{
+			for(int j = 0; j < obstacles[i].length; j++)
+			{
+				obstacles[i][j] = new Obstacle(i*DIMENSION, j*DIMENSION, DIMENSION);
+			}
+		}
+		//Randomize the blocks with the starting obstacle percent
+		randomize(initialPercent);
+	}
+	
+	//Method to randomize the blocks
+	public void randomize(float obstaclePercent)
+	{
+		//Randomize the obstacles based on the obstacle percent
+		for(int i = 0; i < obstacles.length; i++)
+		{
+			for(int j = 0; j < obstacles[i].length; j++)
+			{
+				if(Math.random() < obstaclePercent)
+					obstacles[i][j].enable();
+				else
+					obstacles[i][j].disable();
+			}
+		}
+	}
+	public void draw(Graphics2D g)
+	{
+		//Draw every obstacle in the array
+		for(int i = 0; i < obstacles.length; i++)
+		{
+			for(int j = 0; j < obstacles[i].length; j++)
+			{
+				obstacles[i][j].draw(g);
+			}
+		}
+	}
+	//Method which gets the obstacles
+	public Obstacle[][] getObstacles()
+	{
+		return obstacles;
+	}
+}

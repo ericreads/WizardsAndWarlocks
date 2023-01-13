0080fd7f550d64866d03982737a05519247dac27
Thaneesha Sivasithambaram <thaneesha.sivasithambaram@gmail.com>
2023-01-13 09:04:45 -0500

    Fixed win screen saving

--- src/WinScreen.java
+++ src/WinScreen.java
@@ -14,6 +14,7 @@ public class WinScreen extends GameScreen {
 	private Font dogicaPixelBold;
 	private Font dogicaPixel;
 
+	private int position = 0; 
 	private int frames = 0;
 
 	@Override
@@ -63,42 +64,60 @@ public class WinScreen extends GameScreen {
 		g.setFont(dogicaPixelBold.deriveFont(Font.PLAIN, 22F));
 		g.setColor(brown);
 
-		g.drawString("You Win!", (int) (632 - g.getFont().getStringBounds("You Win!", frc).getWidth() / 2), 285);
+		if (position == 0)
+		{
+			g.drawString("You Win!", (int) (632 - g.getFont().getStringBounds("You Win!", frc).getWidth() / 2), 280);
 
-		g.setFont(dogicaPixel.deriveFont(Font.PLAIN, 14F));
-		text[0] = "Congratulations! You defeated the King";
-		text[1] = "and the citizens of Exalos are free.";
+			g.setFont(dogicaPixel.deriveFont(Font.PLAIN, 14F));
+			text[0] = "Congratulations! You defeated the King";
+			text[1] = "and the citizens of Exalos are free.";
 
-		for (int i = 0; i < text.length; i++) {
-			g.drawString(text[i], (int) (632 - g.getFont().getStringBounds(text[i], frc).getWidth() / 2),
-					325 + (25 * i));
+			for (int i = 0; i < text.length; i++) {
+				g.drawString(text[i], (int) (632 - g.getFont().getStringBounds(text[i], frc).getWidth() / 2),
+						325 + (25 * i));
+			}
+	    
+			
 		}
-    
+		else
+		{
+			g.drawString("Credits", (int) (632 - g.getFont().getStringBounds("Credits", frc).getWidth() / 2), 280);
+			
+			String[] names = {"Eric", "Dev", "Thaneesha"};
+				
+			g.setFont(dogicaPixel.deriveFont(Font.PLAIN, 14F));
+			
+			for (int i = 0; i < 3; i++)
+			{
+				g.drawString("Developer: " + names[i], 430, 310 + (i * 30) );
+			}
+		}
+		
 		g.setFont(dogicaPixelBold.deriveFont(Font.PLAIN, 15F));
-		g.drawString("Press space to",
-				(int) (632 - g.getFont().getStringBounds("Press space to", frc).getWidth() / 2),
-				380);
-		g.drawString("return to the main menu.",
-				(int) (632 - g.getFont().getStringBounds("return to the main menu.", frc).getWidth() / 2),
-				400);
+		g.drawString("Press space to continue.",
+				(int) (632 - g.getFont().getStringBounds("Press space to continue.", frc).getWidth() / 2),
+				400);	
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
-			GameScreenManager.getInstance().clearScreens();
-			GameScreenManager.getInstance().addScreen(new MainMenu());
-		}
+		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_SPACE)
 		{
-			GameScreenManager.getInstance().clearScreens();
-			GameScreenManager.getInstance().addScreen(new MainMenu());
+			if (position == 0)
+			{
+				position++;
+			}
+			else
+			{
+				GameScreenManager.getInstance().clearScreens();
+				GameScreenManager.getInstance().addScreen(new MainMenu());
+			}
 		}
 
 	}
